329203bb7a2a6fb7ba33e0eedb508ecd51a72858
WellSilva <wsilva9589@gmail.com>
2023-06-22 19:20:18 -0300

    alterando msg do IMC e validando numeros

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Gabriela Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
 
@@ -11,6 +11,24 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
 
-tdImc.textContent = imc;
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaValida = false;
+}
+
+if(pesoValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+
+}
