e278bccff672f9c613528c4753b8eb357cd2132c
WellSilva <wsilva9589@gmail.com>
2023-06-22 22:34:20 -0300

    aplicando for para calculo de todos elementos

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Gabriela Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoValido = true;
+    var alturaValida = true;
+    
+    if(peso <= 0 || peso >= 500){
+        console.log("Peso inválido!");
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido!";
+    }
+    
+    if(altura <= 0 || altura >= 3.00){
+        console.log("Altura inválida!");
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida!";
+    }
+    
+    if(pesoValido && alturaValida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 
+}    
 
-
-var pesoValido = true;
-var alturaValida = true;
-
-if(peso <= 0 || peso >= 500){
-    console.log("Peso inválido!");
-    pesoValido = false;
-    tdImc.textContent = "Peso inválido!";
-}
-
-if(altura <= 0 || altura >= 3.00){
-    console.log("Altura inválida!");
-    alturaValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
-
-if(pesoValido && alturaValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-
-}
