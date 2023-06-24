08acf2692e039128c58b0817313ecdb50dd0a65e
WellSilva <wsilva9589@gmail.com>
2023-06-23 21:26:24 -0300

    adicionando novo paciente

--- js/principal.js
+++ js/principal.js
@@ -22,12 +22,14 @@ for(var i = 0; i < pacientes.length; i++){
         console.log("Peso inválido!");
         pesoValido = false;
         tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
     }
     
     if(altura <= 0 || altura >= 3.00){
         console.log("Altura inválida!");
         alturaValida = false;
         tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
     }
     
     if(pesoValido && alturaValida){
