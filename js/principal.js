0d4d8bed99665257ef747474c5acb2403a2606c3
WellSilva <wsilva9589@gmail.com>
2023-06-23 22:03:39 -0300

    completando funcao de adicionar novos pacientes

--- js/principal.js
+++ js/principal.js
@@ -37,6 +37,8 @@ for (var i = 0; i < pacientes.length; i++) {
   }
 }
 
+
+// adicionando botão e função para adicionar pacientes//
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (event) {
   event.preventDefault();
@@ -49,4 +51,26 @@ botaoAdicionar.addEventListener("click", function (event) {
   var gordura = form.gordura.value;
 
   var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");  
+  var pesoTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    pesoTd.textContent = peso;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
 });
