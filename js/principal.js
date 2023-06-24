2a05c66e618dda55456927f4f7d200c0e8e6dd58
WellSilva <wsilva9589@gmail.com>
2023-06-23 21:53:16 -0300

    adicionando pacientes pelo js

--- js/principal.js
+++ js/principal.js
@@ -3,39 +3,50 @@ titulo.textContent = "Gabriela Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i < pacientes.length; i++){
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-    var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
-    
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
-    
-    var tdImc = paciente.querySelector(".info-imc");
-    
-    var pesoValido = true;
-    var alturaValida = true;
-    
-    if(peso <= 0 || peso >= 500){
-        console.log("Peso inválido!");
-        pesoValido = false;
-        tdImc.textContent = "Peso inválido!";
-        paciente.classList.add("paciente-invalido");
-    }
-    
-    if(altura <= 0 || altura >= 3.00){
-        console.log("Altura inválida!");
-        alturaValida = false;
-        tdImc.textContent = "Altura inválida!";
-        paciente.classList.add("paciente-invalido");
-    }
-    
-    if(pesoValido && alturaValida){
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
-    }
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-}    
+  var tdImc = paciente.querySelector(".info-imc");
 
+  var pesoValido = true;
+  var alturaValida = true;
+
+  if (peso <= 0 || peso >= 500) {
+    console.log("Peso inválido!");
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido!";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (altura <= 0 || altura >= 3.0) {
+    console.log("Altura inválida!");
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida!";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (pesoValido && alturaValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var form = document.querySelector("#form-adiciona");
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+});
